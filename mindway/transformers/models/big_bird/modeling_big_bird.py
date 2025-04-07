7c08b7b5ef21df964793431ad05abed2b3f2d04c
liuchuting <liuchuting1@huawei.com>
2025-04-07 10:37:47 +0800

    update big_bird

--- mindway/transformers/models/big_bird/modeling_big_bird.py
+++ mindway/transformers/models/big_bird/modeling_big_bird.py
@@ -250,12 +250,14 @@ class BigBirdEmbeddings(nn.Cell):
         self.dropout = mint.nn.Dropout(config.hidden_dropout_prob)
         # position_ids (1, len position emb) is contiguous in memory and exported when serialized
         self.position_embedding_type = getattr(config, "position_embedding_type", "absolute")
-        self.register_buffer(
-            "position_ids", mint.arange(config.max_position_embeddings).expand((1, -1)), persistent=False
-        )
-        self.register_buffer(
-            "token_type_ids", mint.zeros(self.position_ids.size(), dtype=ms.int64), persistent=False
-        )
+        self.position_ids = mint.arange(config.max_position_embeddings).expand((1, -1))
+        self.token_type_ids =  mint.zeros(self.position_ids.size(), dtype=ms.int64)
+        # self.register_buffer(
+        #     "position_ids", mint.arange(config.max_position_embeddings).expand((1, -1)), persistent=False
+        # )
+        # self.register_buffer(
+        #     "token_type_ids", mint.zeros(self.position_ids.size(), dtype=ms.int64), persistent=False
+        # )
         # End copy
 
         self.rescale_embeddings = config.rescale_embeddings
