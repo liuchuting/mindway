a091f2964c3349cc12b0c3ca51d8a5c6cd3403a1
liuchuting <liuchuting1@huawei.com>
2025-04-08 10:56:34 +0800

    modular_qwen2.5VL

--- mindway/transformers/models/qwen2_5_vl/__init__.py
+++ mindway/transformers/models/qwen2_5_vl/__init__.py
@@ -1,1 +1,8 @@
 from .modeling_qwen2_5_vl import Qwen2_5_VLForConditionalGeneration, Qwen2_5_VLModel, Qwen2_5_VLPreTrainedModel
+from .modular_qwen2_5_vl import (
+    Qwen2_5_VLConfig,
+    Qwen2_5_VLForConditionalGeneration,
+    Qwen2_5_VLModel,
+    Qwen2_5_VLPreTrainedModel,
+    Qwen2_5_VLProcessor
+)
